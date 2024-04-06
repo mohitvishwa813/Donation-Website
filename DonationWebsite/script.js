832fea739c0e907597ceb4daf21c860f763dd56f
mohitvishwa813 <122216178+mohitvishwa813@users.noreply.github.com>
2024-04-06 12:30:50 +0530

    Add files via upload

---
+++ DonationWebsite/script.js
@@ -0,0 +1,9 @@
+const btn=document.querySelector(".donate");
+btn.addEventListener('click',()=>{
+window.open("https://imjo.in/G6y72x", "_blank");
+})
+
+const btn1=document.querySelector(".donate1");
+btn1.addEventListener('click',()=>{
+window.open("https://imjo.in/G6y72x", "_blank");
+})
